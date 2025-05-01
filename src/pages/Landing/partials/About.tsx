6d1bbe289756087d55a915513d3d8681f937e134
Ww <nickolaswewe2@gmail.com>
2025-05-01 14:13:16 +0700

    Fe Template

--- src/pages/Landing/partials/About.tsx
+++ src/pages/Landing/partials/About.tsx
@@ -8,10 +8,10 @@ const About = () => {
       <div className='flex'> 
         <div className='w-2/3 flex flex-col gap-6 justify-center items-center h-screen z-10'> 
         <h1 className='text-5xl font-bold bg-gradient-to-r from-red-500 via-blue-500 to-yellow-500 text-transparent bg-clip-text'>
-          What is GASC ??
+          What is GDGOC ??
         </h1>
         <p className='w-1/2 text-lg text-justify'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate aspernatur dolores a nulla vel debitis laudantium facilis magnam dignissimos? Vitae accusamus recusandae dolores error quasi? Odio veniam sed ducimus reprehenderit!
+        GDGoC (Google Developer Groups on Campus) adalah komunitas mahasiswa global yang didukung oleh Google, merupakan rebranding dari GDSC sejak tahun 2024. Tujuan utama GDGoC adalah menjembatani teori yang dipelajari di kampus dengan praktik industri teknologi melalui kegiatan seperti workshop, hackathon, dan proyek kolaboratif. Komunitas ini membantu mahasiswa mengembangkan keterampilan teknis, kepemimpinan, serta membangun jaringan profesional di bidang teknologi.
         </p>
         </div>
         <div className=''> 
